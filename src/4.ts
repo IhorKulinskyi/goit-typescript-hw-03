cedffa25a733ab1c4e6a54a3d6567bf299ee7a99
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-10-26 17:44:02 +0300

    done tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +53,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
